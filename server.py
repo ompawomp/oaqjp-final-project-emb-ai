a375595396982d06a0272caa227cb8d188738d63
ompawomp <wpmock@hotmail.com>
2026-01-13 15:29:37 -0500

    updates thru end of final project

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
+'''import needed components from the flask library'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 @app.route("/emotionDetector")
 def emotion_analysis():
+    '''Retrieve text, pass to emotion_detector() and return output'''
     text_to_analyze = request.args.get('textToAnalyze', '')
     emotion_result = emotion_detector(text_to_analyze)
     anger = emotion_result['anger']
@@ -12,13 +14,18 @@ def emotion_analysis():
     joy = emotion_result['joy']
     sadness = emotion_result['sadness']
     dominant_emotion = emotion_result['dominant_emotion']
+    if dominant_emotion is None:
+        return(
+            "Invalid text! Please try again!"
+        )
     return (
-        f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}.")
-
-         
+        f"For the given statement, the system response is 'anger': {anger}, \
+        'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. \
+        The dominant emotion is {dominant_emotion}.")
 @app.route("/")
-    render_index_page():
-    return render_template('index.html')
+def render_index_page():
+    '''generates HTML based on dynamic input and index.html'''
+    return render_template('index.html')
 
-    _name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
